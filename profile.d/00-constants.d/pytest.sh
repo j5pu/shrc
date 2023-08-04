0d07a27fcad48c2fa1bcf098013a5808bb567d1b
José <63794670+j5pu@users.noreply.github.com>
2023-08-04 22:08:14 +0200

    feat: jetbrains

---
+++ profile.d/00-constants.d/pytest.sh
@@ -0,0 +1,7 @@
+# shellcheck shell=sh
+
+# https://docs.pytest.org/en/stable/reference/reference.html#envvar-PYTEST_THEME_MODE
+#
+export PYTEST_THEME="github-dark"
+export PYTEST_THEME_MODE="dark"
+
