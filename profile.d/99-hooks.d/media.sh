557b1e79c15786d7f31e4b3f7b0d9f6452ae8fd6
José <63794670+j5pu@users.noreply.github.com>
2023-08-07 11:53:41 +0200

    auto

--- profile.d/99-hooks.d/media.sh
+++ profile.d/99-hooks.d/media.sh
@@ -3,3 +3,4 @@
 export MEDIA_PROJECT="${HOME}/media"
 export PATH="${MEDIA_PROJECT}/bin:${MEDIA_PROJECT}/scripts:${MEDIA_PROJECT}/scripts/run:${PATH}"
 export PYTHONPATH="${MEDIA_PROJECT}${PYTHONPATH:+:${PYTHONPATH}}"
+
