7ac658703c34c58f6c99928d21da425698b3e357
root <root@local>
2023-08-07 17:15:51 +0200

    auto

--- profile.d/00-configs.d/git.sh
+++ profile.d/00-configs.d/git.sh
@@ -1,4 +1,6 @@
 # shellcheck shell=sh
 
+# Git Command Line Tools ignores GIT_CONFIG_SYSTEM and GIT_CONFIG_GLOBAL
+#
 export GIT_CONFIG_SYSTEM="${SHRC_CONFIG}/git/gitconfig"
 export GIT_TEMPLATE_DIR="${SHRC_CONFIG}/git/template"
